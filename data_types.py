e4d9236ba88a618e1e1f8a8458bfa9c1a66c5f1c
notitguy <notitguy@email.com>
2024-02-23 00:14:34 +0100

    chapter 4

---
+++ data_types.py
@@ -0,0 +1,38 @@
+meaning = 42
+# Ternary
+# print('Right on!') if meaning > 10 else print('Not today')
+
+# constructor function
+# pizza = str('Pepperoni')
+# print(type(pizza))
+# print(type(pizza) == str)
+# print(isinstance(pizza, str))
+
+first = 'James'
+last = 'Rodriguez'
+
+# concatenation
+fullname = first + ' ' + last
+fullname += '!'
+# print(fullname)
+
+# mutltiine
+multiline = '''
+Hey there
+What u doing?
+          All good?
+'''
+# print(multiline)
+
+# escape spec. charac.
+
+sentence = 'I\'m back at work!\tHey!\n\nWhere\'s this at\\located?'
+# print(sentence)
+
+#  string methods
+# print(first)
+# print(first.lower())
+# print(first.upper())
+# print(first)
+
+# https://youtu.be/H2EJuAcrZYU?t=3450
